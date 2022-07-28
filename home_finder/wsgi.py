36a65c63fd14de4a8ce9b7ee78a45e981bc61073
Thomas Leese <thomas@leese.io>
2022-07-28 16:11:37 +0100

    Reformat code according to Black

--- home_finder/wsgi.py
+++ home_finder/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_finder.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home_finder.settings")
 
 application = get_wsgi_application()
