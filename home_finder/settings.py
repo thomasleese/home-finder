26a95b747142bbe2a31349ebd44eef0399d06715
Thomas Leese <thomas@leese.io>
2022-07-28 16:28:21 +0100

    Set locale

--- home_finder/settings.py
+++ home_finder/settings.py
@@ -103,12 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "en-gb"
+TIME_ZONE = "Europe/London"
 
 USE_I18N = True
-
 USE_TZ = True
 
 
