b8adf21abb45a696de82eaee902bef46ca52efd3
Thomas Leese <thomas@leese.io>
2022-07-29 15:12:28 +0100

    Create initial search models

--- searches/admin.py
+++ searches/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Search, TravelTimeRequirement
+
+
+class TravelTimeRequirementInline(admin.StackedInline):
+    model = TravelTimeRequirement
+    raw_id_fields = ("place",)
+
+
+@admin.register(Search)
+class SearchAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "max_price_sale",
+        "max_price_rent_per_week",
+        "max_price_rent_per_month",
+    )
+    inlines = (TravelTimeRequirementInline,)
--- searches/models.py
+++ searches/models.py
@@ -1,3 +1,55 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from properties.models import Place
+
+
+class Search(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="searches"
+    )
+
+    max_price_sale = models.PositiveIntegerField(null=True, blank=True)
+    max_price_rent_per_week = models.PositiveIntegerField(null=True, blank=True)
+    max_price_rent_per_month = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "searches"
+
+
+class TravelTimeRequirement(models.Model):
+    search = models.ForeignKey(
+        Search, on_delete=models.CASCADE, related_name="travel_time_requirements"
+    )
+
+    place = models.ForeignKey(
+        Place, on_delete=models.CASCADE, related_name=None, null=True, blank=True
+    )
+    place_kind = models.CharField(max_length=32, choices=Place.Kind.choices, blank=True)
+
+    class Mode(models.TextChoices):
+        DRIVING = "driving", _("Driving")
+        CYCLING = "cycling", _("Cycling")
+        WALKING = "walking", _("Walking")
+        TRANSIT = "transit", _("Public transport")
+
+    mode = models.CharField(max_length=8, choices=Mode.choices)
+
+    max_distance = models.PositiveBigIntegerField(null=True, blank=True)
+    max_duration = models.PositiveBigIntegerField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(
+                    place__isnull=False,
+                    place_kind__isnull=True,
+                )
+                | models.Q(
+                    place__isnull=True,
+                    place_kind__isnull=False,
+                ),
+                name="valid_place_or_place_kind",
+            ),
+        ]
